79f305b6f3570ad2c7626e6d1b57bab26857ec13
Riley Larson <byufan2018@byui.edu>
2023-10-28 11:04:14 -0600

    Added server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express =require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {res.send("Hello Browser")});
+
+app.get('/riley', (req,res) => {res.send("Hello Riley")});
+
+app.listen(port, ()=>console.log("Listening"));
